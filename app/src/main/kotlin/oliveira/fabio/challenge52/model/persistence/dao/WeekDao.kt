f0466704b44d1a6bd1f70117c6f3aeee56fb5314
Fabio Oliveira <fabiomartins@ext.cvc.com.br>
2019-04-02 19:15:12 -0300

    OnLongClick and click Flux

--- app/src/main/kotlin/oliveira/fabio/challenge52/model/persistence/dao/WeekDao.kt
+++ app/src/main/kotlin/oliveira/fabio/challenge52/model/persistence/dao/WeekDao.kt
@@ -1,15 +1,15 @@
 package oliveira.fabio.challenge52.model.persistence.dao
 
-import androidx.room.Dao
-import androidx.room.Insert
-import androidx.room.OnConflictStrategy
-import androidx.room.Update
+import androidx.room.*
 import oliveira.fabio.challenge52.model.entity.Week
 
 @Dao
 interface WeekDao {
     @Insert(onConflict = OnConflictStrategy.REPLACE)
     fun addWeeks(weeks: List<Week>): List<Long>
+
+    @Delete
+    fun deleteWeeks(weeksList: List<Week>): Int
 
     @Update
     fun updateWeek(week: Week)
