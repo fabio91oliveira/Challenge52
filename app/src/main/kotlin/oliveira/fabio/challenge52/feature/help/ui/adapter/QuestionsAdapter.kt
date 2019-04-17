6dbf6f37c69e911bb3cd0d9fd6e75b0a6344d9b5
Fabio Oliveira <fabiomartins@ext.cvc.com.br>
2019-04-17 17:48:37 -0300

    HelpFragment adjustments layout

--- app/src/main/kotlin/oliveira/fabio/challenge52/feature/help/ui/adapter/QuestionsAdapter.kt
+++ app/src/main/kotlin/oliveira/fabio/challenge52/feature/help/ui/adapter/QuestionsAdapter.kt
@@ -1,8 +1,17 @@
 package oliveira.fabio.challenge52.feature.help.ui.adapter
 
+import android.animation.ArgbEvaluator
+import android.animation.ObjectAnimator
+import android.animation.ValueAnimator
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.view.animation.AccelerateDecelerateInterpolator
+import android.view.animation.AlphaAnimation
+import android.view.animation.AnimationSet
+import android.view.animation.DecelerateInterpolator
+import androidx.appcompat.widget.AppCompatTextView
+import androidx.core.content.ContextCompat
 import androidx.recyclerview.widget.RecyclerView
 import kotlinx.android.extensions.LayoutContainer
 import kotlinx.android.synthetic.main.item_question.*
@@ -13,6 +22,7 @@ import oliveira.fabio.challenge52.feature.help.vo.Question
 class QuestionsAdapter :
     RecyclerView.Adapter<QuestionsAdapter.ViewHolder>() {
     private var list: MutableList<Question> = mutableListOf()
+    private var lastPosition = 0
 
     override fun getItemCount() = list.size
     override fun onBindViewHolder(holder: ViewHolder, position: Int) = holder.bind(list[position])
@@ -32,24 +42,84 @@ class QuestionsAdapter :
             txtAnswer.text = question.answer
 
             if (!question.isOpen) {
+                txtQuestion.setTextColor(ContextCompat.getColor(containerView.context, R.color.colorBlack))
                 txtAnswer.visibility = View.GONE
-                imgArrow.rotation = 180f
+                btnAction.setBackgroundResource(R.drawable.ic_add_circle)
             } else {
+                txtQuestion.setTextColor(ContextCompat.getColor(containerView.context, R.color.colorAccent))
                 txtAnswer.visibility = View.VISIBLE
-                imgArrow.rotation = 0f
+                btnAction.setBackgroundResource(R.drawable.ic_remove_circle)
             }
+
+            if (adapterPosition >= lastPosition) animate()
 
             containerView.setOnClickListener {
                 if (question.isOpen) {
-                    imgArrow.animate().rotation(180f)
+                    btnAction.setBackgroundResource(R.drawable.ic_add_circle)
+                    fadeText(txtAnswer, 1f, 0f, 200)
                     txtAnswer.visibility = View.GONE
                     question.isOpen = false
+                    changeTextQuestionColor(
+                        ContextCompat.getColor(containerView.context, R.color.colorAccent),
+                        ContextCompat.getColor(containerView.context, R.color.colorBlack)
+                    )
+
                 } else {
-                    imgArrow.animate().rotation(0f)
+                    btnAction.setBackgroundResource(R.drawable.ic_remove_circle)
                     txtAnswer.visibility = View.VISIBLE
+                    fadeText(txtAnswer, 0f, 1f, 700)
+                    transformationY(txtAnswer, -50f, 0f, 200)
+                    transformationY(viewDivisor, -50f, 0f, 200)
                     question.isOpen = true
+                    changeTextQuestionColor(
+                        ContextCompat.getColor(containerView.context, R.color.colorBlack),
+                        ContextCompat.getColor(containerView.context, R.color.colorAccent)
+                    )
                 }
             }
+        }
+
+        private fun changeTextQuestionColor(firstColor: Int, secondColor: Int) = ObjectAnimator.ofObject(
+            txtQuestion,
+            "textColor",
+            ArgbEvaluator(),
+            firstColor,
+            secondColor
+        )
+            .setDuration(250)
+            .start()
+
+        private fun fadeText(textView: AppCompatTextView, from: Float, to: Float, duration: Long) {
+            val fadeIn = AlphaAnimation(from, to)
+            fadeIn.interpolator = DecelerateInterpolator()
+            fadeIn.duration = duration
+
+            val animation = AnimationSet(false)
+            animation.addAnimation(fadeIn)
+            textView.animation = animation
+        }
+
+        private fun transformationY(view: View, from: Float, to: Float, duration: Long) {
+            val valueAnimator = ValueAnimator.ofFloat(from, to)
+            valueAnimator.interpolator = AccelerateDecelerateInterpolator()
+            valueAnimator.duration = duration
+            valueAnimator.addUpdateListener {
+                val progress = it.animatedValue as Float
+                view.translationY = progress
+            }
+            valueAnimator.start()
+        }
+
+        private fun animate() {
+            val valueAnimator = ValueAnimator.ofFloat(-500f, 0f)
+            valueAnimator.interpolator = AccelerateDecelerateInterpolator()
+            valueAnimator.duration = 500
+            valueAnimator.addUpdateListener {
+                val progress = it.animatedValue as Float
+                containerView.translationX = progress
+            }
+            valueAnimator.start()
+            lastPosition++
         }
 
     }
