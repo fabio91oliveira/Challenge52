3911ce86aec82511ddfdc14ab46d17d67be83c36
Fabio Oliveira <fabio91oliveira@gmail.com>
2020-04-19 18:46:18 -0300

    Fix Timber

--- app/src/main/kotlin/oliveira/fabio/challenge52/Challenge52Application.kt
+++ app/src/main/kotlin/oliveira/fabio/challenge52/Challenge52Application.kt
@@ -2,10 +2,21 @@ package oliveira.fabio.challenge52
 
 import android.app.Application
 import oliveira.fabio.challenge52.di.KoinStarter
+import timber.log.Timber
 
 class Challenge52Application : Application() {
     override fun onCreate() {
         super.onCreate()
+        startTimber()
+        startKoin()
+    }
+
+    private fun startTimber() {
+        if (BuildConfig.DEBUG)
+            Timber.plant(Timber.DebugTree())
+    }
+
+    private fun startKoin() {
         KoinStarter.start(this)
     }
 }
